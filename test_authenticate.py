c8de2f27f35a71463e8de1b4b370efcdb31ee949
brendand <brendan.j.donegan@gmail.com>
2017-05-03 19:34:00 +0200

    Rest of tests and requirements

--- test_authenticate.py
+++ test_authenticate.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import testtools
 
@@ -16,7 +17,7 @@ AUTHENTICATE_URI = OPENAM_URI + "/authenticate"
 OPENAM_USERNAME_HEADER = "X-OpenAM-Username"
 OPENAM_PASSWORD_HEADER = "X-OpenAM-Password"
 
-class TestAuthenticate(testtools.TestCase):
+class TestAuthenticateZeroPage(testtools.TestCase):
 
     def test_authenticate_default(self):
         """
@@ -63,3 +64,112 @@ class TestAuthenticate(testtools.TestCase):
         }
         response = requests.post(AUTHENTICATE_URI, headers=headers)
         self.assertThat(response.status_code, Equals(401))
+
+
+class TestAuthenticateCallbacks(testtools.TestCase):
+
+    def test_authenticate_default(self):
+        """
+        Verify that when we send a POST request to the authenticate endpoint
+        with no username and password specified, we get a callback response,
+        then when we respond to that with the callback filled in with the
+        correct username and password, a response is returned with the tokenId
+        """
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(AUTHENTICATE_URI, headers=headers)
+        self.assertThat(response.status_code, Equals(200))
+        data = response.json()
+        for idx, callback in enumerate(data['callbacks']):
+            if callback['type'] == 'NameCallback':
+                data['callbacks'][idx]['input'][0]['value'] = USERNAME
+            if callback['type'] == 'PasswordCallback':
+                data['callbacks'][idx]['input'][0]['value'] = PASSWORD
+        response = requests.post(
+            AUTHENTICATE_URI,
+            data=json.dumps(data),
+            headers=headers,
+        )
+        self.assertThat(response.status_code, Equals(200))
+        self.assertThat(response.json(), KeysEqual(
+            {
+                "successUrl": "",
+                "tokenId": "",
+            }
+        ))
+
+    def test_authenticate_invalid_username(self):
+        """
+        Verify that when we send a POST request to the authenticate endpoint
+        with no username and password specified, we get a callback response,
+        then when we respond to that with the callback filled in with the
+        incorrect username and correct password, a 401 response is returned.
+        """
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(AUTHENTICATE_URI, headers=headers)
+        self.assertThat(response.status_code, Equals(200))
+        data = response.json()
+        for idx, callback in enumerate(data['callbacks']):
+            if callback['type'] == 'NameCallback':
+                data['callbacks'][idx]['input'][0]['value'] = 'invalid'
+            if callback['type'] == 'PasswordCallback':
+                data['callbacks'][idx]['input'][0]['value'] = PASSWORD
+        response = requests.post(
+            AUTHENTICATE_URI,
+            data=json.dumps(data),
+            headers=headers,
+        )
+        self.assertThat(response.status_code, Equals(401))
+
+    def test_authenticate_invalid_password(self):
+        """
+        Verify that when we send a POST request to the authenticate endpoint
+        with no username and password specified, we get a callback response,
+        then when we respond to that with the callback filled in with the
+        correct username and incorrect password, a 401 response is returned.
+        """
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(AUTHENTICATE_URI, headers=headers)
+        self.assertThat(response.status_code, Equals(200))
+        data = response.json()
+        for idx, callback in enumerate(data['callbacks']):
+            if callback['type'] == 'NameCallback':
+                data['callbacks'][idx]['input'][0]['value'] = USERNAME
+            if callback['type'] == 'PasswordCallback':
+                data['callbacks'][idx]['input'][0]['value'] = 'invalid'
+        response = requests.post(
+            AUTHENTICATE_URI,
+            data=json.dumps(data),
+            headers=headers,
+        )
+        self.assertThat(response.status_code, Equals(401))
+
+    def test_authenticate_no_session(self):
+        """
+        Verify that when we send a POST request to the authenticate endpoint
+        with no username and password specified, we get a callback response,
+        then when we respond to that with the callback filled in with the
+        correct username and correct password and the noSession parameter set
+        to true, then a response is returned without the tokenId
+        """
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(AUTHENTICATE_URI, headers=headers)
+        self.assertThat(response.status_code, Equals(200))
+        data = response.json()
+        for idx, callback in enumerate(data['callbacks']):
+            if callback['type'] == 'NameCallback':
+                data['callbacks'][idx]['input'][0]['value'] = USERNAME
+            if callback['type'] == 'PasswordCallback':
+                data['callbacks'][idx]['input'][0]['value'] = PASSWORD
+        response = requests.post(
+            AUTHENTICATE_URI,
+            data=json.dumps(data),
+            headers=headers,
+            params={'noSession': True},
+        )
+        self.assertThat(response.status_code, Equals(200))
+        self.assertThat(response.json(), KeysEqual(
+            {
+                "successUrl": "",
+                "message": "",
+            }
+        ))
